5ccfe61422043df47b6eb28fd56139d1981c1625
JaredMorillo <jared.morillo@hotmail.com>
2025-02-17 12:04:02 -0500

    Initial Commit

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -76,4 +76,45 @@ validate.checkRegData = async (req, res, next) => {
     next()
 }
 
+validate.loginRules = () => {
+    return [
+        body("account_email")
+        .trim()
+        .isEmail()
+        .normalizeEmail() // refer to validator.js docs
+        .withMessage("A valid email is required."),
+
+        body("account_password")
+        .trim()
+        .isStrongPassword({
+            minLength: 12,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        })
+        .withMessage("Password does not meet the requirements."),
+    ]
+}
+
+validate.checkLoginData = async (req, res, next) => {
+    const { account_email, account_password } =
+    req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        let nav = await utilities.getNav()
+        res.render("account/login", {
+            errors,
+            title:"Login",
+            nav, 
+            account_email,
+            account_password,
+        })
+        return
+    }
+    next()
+}
+
+
 module.exports = validate
